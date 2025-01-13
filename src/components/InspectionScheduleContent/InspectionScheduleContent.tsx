2a85d4e7416746155e9b981ee71b6a16868efb4c
Matthew Seah <matthewseah17@gmail.com>
2025-01-13 15:04:56 +0800

    remove testing comment

--- src/components/InspectionScheduleContent/InspectionScheduleContent.tsx
+++ src/components/InspectionScheduleContent/InspectionScheduleContent.tsx
@@ -37,8 +37,6 @@ import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import Tooltip from "@mui/material/Tooltip";
 import { usePremises } from "../../contexts/PremisesContext";
 
-//testing
-
 interface InspectionScheduleContentProps {
   onAssignModeChange: (isAssignMode: boolean) => void;
 }
